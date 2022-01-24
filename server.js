3db7c98dc99436f34f54978d0f39bc9b1a8e004b
mosaab-rady <70973036+mosaab-rady@users.noreply.github.com>
2022-01-24 14:47:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res,next) => {
+app.get('/', (req, res, next) => {
   res.send('hello world my name is mosaab');
   res.end();
 });
